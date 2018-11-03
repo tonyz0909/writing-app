3a878ad27c2b82e3ea29256f058695f1f77fa407
tonyz0909 <tonyz0909@gmail.com>
2018-11-03 16:09:21 -0500

    implemented parse textarea

--- app/static/js/parse.js
+++ app/static/js/parse.js
@@ -1,11 +1,7 @@
-$(document).ready(function () {
-
-});
-
 document.getElementById("submit").onclick = function() { parseText() };
 
-function myFunction() {
+function parseText() {
   text = document.getElementById('exampleFormControlTextarea1').value;
-  console.log(text)
-  document.getElementById(results).innerHTML = text;
+  console.log(text);
+  document.getElementById("results").innerHTML = text;
 }
